0078fa60bd11810548ecd4c19f47c62023f599a9
VarunyThePruney <122654699+VarunyThePruney@users.noreply.github.com>
2025-03-07 13:39:21 +0530

    Add files via upload
    added pdf download button along with dynamic naming of pdf

--- medical-chatbot/hash_pass.py
+++ medical-chatbot/hash_pass.py
@@ -1,7 +1,7 @@
 import streamlit_authenticator as stauth
 
 # List of passwords to hash
-passwords = ["password123", "password123"]
+passwords = ["password123", "hello!123"]
 
 # Correct usage of Hasher in v0.4.2
 hasher = stauth.Hasher(passwords)  # Initialize first
